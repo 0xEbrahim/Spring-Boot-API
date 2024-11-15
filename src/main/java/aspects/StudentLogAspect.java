1acd271a6d902e11c1aa13af4de096dfead26531
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-15 20:02:03 +0200

    comment: starting dev on a new branch

--- src/main/java/aspects/StudentLogAspect.java
+++ src/main/java/aspects/StudentLogAspect.java
@@ -1,12 +1,10 @@
 package aspects;
 
-import org.aspectj.lang.JoinPoint;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
